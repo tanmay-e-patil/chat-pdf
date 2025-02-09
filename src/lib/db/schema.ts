30f42e5336832d8cb5f6791f064d80d3e882d244
Tanmay Patil <patil.tanmay0697@gmail.com>
2025-02-08 18:02:16 -0800

    Setup a schema for chats and messages

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,21 @@
+import {pgEnum, pgTable, text, timestamp, uuid, varchar} from 'drizzle-orm/pg-core'
+
+
+export const userSystemEnum = pgEnum('user_system_enum', ['assistant', 'user'])
+
+export const chats = pgTable('chats', {
+    id: uuid('id').primaryKey(),
+    pdfName: text('pdf_name').notNull(),
+    pdfUrl: text('pdf_url').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    userId: varchar('user_id', {length:256}).notNull(),
+    fileKey: text('file_key').notNull(),
+})
+
+export const messages = pgTable('messages', {
+    id: uuid('id').primaryKey(),
+    chatId: uuid('chat_id').references(() => chats.id).notNull(),
+    message: text('message').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    userId: varchar('user_id', {length:256}).notNull(),
+})
