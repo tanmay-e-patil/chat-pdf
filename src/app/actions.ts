89dfe261a066360fe48056f2b6fda9c46bfef2b2
Tanmay Patil <patil.tanmay0697@gmail.com>
2025-02-08 18:14:30 -0800

    Fix drizzle config

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,7 @@
 import { neon } from "@neondatabase/serverless";
 
 export async function getData() {
-    const sql = neon(process.env.DATABASE_URL);
+    const sql = neon(process.env.DATABASE_URL!);
     const data = await sql`...`;
     return data;
 }
