516cbee8ea02459a669cc7c78f39fc4b4aafb658
Tanmay Patil <patil.tanmay0697@gmail.com>
2025-02-11 12:16:54 -0800

    Fix dynamic api route

--- src/app/api/chat/[chatId]/messages/route.ts
+++ src/app/api/chat/[chatId]/messages/route.ts
@@ -3,13 +3,16 @@ import { messages } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
-export const GET = async (req: Request, context) => {
+export const GET = async (
+  req: Request,
+  context: { params: { chatId: string } },
+) => {
   const { params } = context;
-  const chatId = await params.chatId;
+  const chatId = params.chatId;
   const _messages = await db
     .select()
     .from(messages)
-    .where(eq(chatId, messages.chatId))
+    .where(eq(messages.chatId, chatId))
     .execute();
   console.log(_messages);
   return NextResponse.json(_messages, { status: 200 });
